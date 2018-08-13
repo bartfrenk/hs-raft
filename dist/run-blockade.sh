38ecee3c98966f6e308918181f643e7df1614772
Bart Frenk <bart.frenk@gmail.com>
2018-08-13 22:05:15 +0200

    Finish setup for running raft-control in a container

---
+++ dist/run-blockade.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+containers=$(docker ps --format '{{.Names}}')
+# echo "$containers"
+
+for c in $containers;
+do
+    if [[ "$c" =~ raft-[0-9]+ ]]; then
+        blockade add "$c";
+        echo "Added container $c to the blockade"
+    fi
+done;
